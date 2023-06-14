c6ac1f0e70f9c8ada3687692d129c3d045bcda10
Cody McKeon <codymckeon@gmail.com>
2023-06-14 16:41:39 -0700

    changes to file.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # AlchemyTees
+
+GitHub Commands
+
+1. git branch
+2. git checkout branchName
+3. git checkout -b branchName
+4. git status
+5. git add .
+6. git commit -m "Commit"
+7. git push -u origin branchName
+
+8. List Products
+   1. Create products array
+   2. add product images
+   3. render products
+   4. style products
---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import data from './data';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Alchemy Tees</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>$</strong>
+                  {product.price}
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
